d262dabae3e48a1ea05188bc0ad8b43a4e56e2e9
Dihep <dihep.demayo@gmail.com>
2024-09-10 23:48:43 +0200

    Frontpage funzionante ma carente stile.

--- src/routes/AppMain.vue
+++ src/routes/AppMain.vue
@@ -4,7 +4,7 @@ import store from '../data/store.js';
 import { RouterLink } from 'vue-router';
 
 export default {
-  name: 'AppMain',
+  name: 'AppSearchFilters',
 
   components: {},
 
@@ -12,46 +12,31 @@ export default {
     return {
       store,
       searchData: null,
-      inputCittà: '',
-      coordinates: [],
+      inputIndirizzo: '',
+      inputCamere: 1,
+      inputLetti: 1,
+      inputServizi: [],
+      inputRaggio: 20,
+      rangeStyle: '',
       apiKey: 'RUfkTtEK0CYbHBG3YE2RSEslSRGAWZcu',
       apiSuggestions: [],
-      contentSize: 10,
-      homepageContent: [],
-      isLoading: false,
       isActive: false,
-      inputRaggio: 20,
+      coordinates: [],
       visible: false,
       errors: null,
+      isActiveStanze: false,
     };
   },
 
-
   watch: {
-    //Animazione di caricamento
-    'store.searchApartment'() {
-      this.isLoading = true;
-      setTimeout(() => {
-        this.isLoading = false;
-      }, 2000);
-    },
-    //Assistenza input
-    inputCittà(newInputCittà, oldInputCittà) {
-      if (newInputCittà.length > 0 && this.isActive == true) {
-        this.assistenzaCittà(newInputCittà);
+    inputIndirizzo(newInputIndirizzo, oldInputIndirizzo) {
+      if (newInputIndirizzo.length > 0 && this.isActive == true) {
+        this.assistenzaIndirizzo(newInputIndirizzo);
       }
     },
   },
 
-
   methods: {
-
-    //Funzioni di grafica
-    timeoutShow() {
-      setTimeout(() => {
-        this.isActive = false;
-      }, 200);
-    },
     updateRangeBackground() {
       const min = 1;
       const max = 100;
@@ -63,47 +48,42 @@ export default {
       );
     },
 
-    //Funzioni per il funzionamento interno
-    getImage(immagine) {
-      if (immagine.startsWith('http')) {
-        return immagine;
-      } else {
-        return `http://127.0.0.1:8000/storage/${immagine}`;
-      }
-    },
-    apartmentDetail(appartamento) {
-      this.store.apartmentDetail = appartamento;
-      console.log(this.store.apartmentDetail);
+    toggleStanze() {
+      this.isActiveStanze = !this.isActiveStanze;
     },
 
-    //Correzione testi e recupero dati
-    getRightWords(parola, appartamento) {
-      if (parola == 'Camere' && appartamento == 1) {
-        return 'Camera';
-      } else if (parola == 'Camere') {
-        return parola;
-      }
-      if (parola == 'Posti letto' && appartamento == 1) {
-        return 'Posto letto';
-      } else if (parola == 'Posti letto') {
-        return parola;
-      }
-      if (parola == 'Bagni' && appartamento == 1) {
-        return 'Bagno';
-      } else if (parola == 'Bagni') {
-        return parola;
-      }
-    },
-    getServizio(id) {
-      return this.store.servizi_bnb[id - 1];
+    plusButtonStanze() {
+      this.inputCamere++;
     },
 
-    //Funzioni con contatto API
-    assistenzaCittà(città) {
+    minusButtonStanze() {
+      if (this.inputCamere > 1) {
+        this.inputCamere--;
+      }
+    },
+
+    plusButtonLetti() {
+      this.inputLetti++;
+    },
+
+    minusButtonLetti() {
+      if (this.inputLetti > 1) {
+        this.inputLetti--;
+      }
+    },
+
+    assistenzaIndirizzo(indirizzo) {
+      // const url_tomtom = `https://api.tomtom.com/search/2/search/${encodeURIComponent(
+      //   indirizzo
+      // )}.json?key=${
+      //   this.apiKey
+      // }&typeahead=true&limit=5&countrySet=IT&idxSet=Geo`;
 
       const url_tomtom = `https://api.tomtom.com/search/2/geocode/${encodeURIComponent(
-        città
-      )}.json?key=${this.apiKey}&typeahead=true&limit=5&countrySet=IT&entityTypeSet=Municipality`;
+        indirizzo
+      )}.json?key=${this.apiKey}&typeahead=true&limit=5&countrySet=IT`;
+
+      // ricerca axios
 
       axios.get(url_tomtom).then((response) => {
         let apiResponse = response.data.results;
@@ -112,43 +92,58 @@ export default {
         for (let i = 0; i < apiResponse.length; i++) {
           newSuggestions.push(apiResponse[i].address.freeformAddress);
         }
-        console.log("Risposta API: ", newSuggestions);
+
         this.apiSuggestions = newSuggestions;
       });
     },
+
     writeAddress(actualAddress, event) {
       event.stopPropagation();
-      this.inputCittà = actualAddress;
+      this.inputIndirizzo = actualAddress;
       this.isActive = false;
     },
+
+    timeoutShow() {
+      setTimeout(() => {
+        this.isActive = false;
+      }, 150);
+    },
+
     searchRequest() {
       this.errors = [];
-      if (!this.InputCittà) {
+      if (!this.inputIndirizzo) {
         this.visible = true;
         // this.store.searchApartment = [];
       } else {
         this.visible = false;
         axios
           .get(
-            `https://api.tomtom.com/search/2/geocode/${this.InputCittà}.json?key=RUfkTtEK0CYbHBG3YE2RSEslSRGAWZcu&countrySet=IT&entityTypeSet=Municipality`
+            `https://api.tomtom.com/search/2/geocode/${this.inputIndirizzo}.json?key=RUfkTtEK0CYbHBG3YE2RSEslSRGAWZcu&countrySet=IT`
           )
           .then((response) => {
             this.coordinates = response.data.results[0].position;
+            console.log(this.searchData);
 
             this.$router.push({
               name: 'search',
               query: {
-                indirizzo: this.InputCittà,
+                indirizzo: this.inputIndirizzo,
                 latitude: this.coordinates.lat,
                 longitude: this.coordinates.lon,
-                radius: 20,
+                radius: this.inputRaggio,
+                beds: this.inputLetti,
+                rooms: this.inputCamere,
+                services: this.inputServizi.join(','),
               },
             });
 
             const url = `http://127.0.0.1:8000/api/search?latitude=${
               this.coordinates.lat
-              }&longitude=${this.coordinates.lon
-              }&radius=${this.inputRaggio}`;
+            }&longitude=${this.coordinates.lon}&radius=${
+              this.inputRaggio
+            }&beds=${this.inputLetti}&rooms=${
+              this.inputCamere
+            }&services=${this.inputServizi.join(',')}`;
 
             console.log(url);
 
@@ -165,20 +160,26 @@ export default {
           });
       }
     },
-
+    loadData() {
+      this.inputIndirizzo = this.$route.query.indirizzo;
+      this.inputCamere = this.$route.query.rooms ? this.$route.query.rooms : 1;
+      this.inputLetti = this.$route.query.beds ? this.$route.query.beds : 1;
+      this.inputRaggio = this.$route.query.radius
+        ? this.$route.query.radius
+        : 20;
+      if (this.$route.query.services) {
+        this.inputServizi = this.$route.query.services.split(',').map(Number);
+      }
+    },
   },
-
-
   mounted() {
-
     this.updateRangeBackground();
+    this.loadData();
     const url = `http://127.0.0.1:8000/api/apartments`;
-    //Chiamata dati per appartamenti
     axios
       .get(url)
       .then((response) => {
         this.store.homepageContent = response.data.results.data;
-        console.log(response);
         console.log(this.store.homepageContent);
       })
       .catch((error) => {
@@ -189,165 +190,136 @@ export default {
 </script>
 
 <template>
-  <div class="search-container">
-    <div class="search-bar">
-      <!-- Input città -->
-      <div class="suggerimenti-città">
-        <input class="" id="città" list="suggestion" type="text" placeholder="Inserisci una città" v-model="inputCittà"
-          @focus="isActive = true" @blur="timeoutShow" autocomplete="off" />
-        <!-- Suggerimenti input -->
-        <ul v-show="apiSuggestions.length > 0 && isActive == true">
-          <li @click="writeAddress(singleAddress, $event)" v-for="(singleAddress, i) in apiSuggestions">
-            <i class="fa-solid fa-location-dot"></i>
-            <span>{{ singleAddress }}</span>
-          </li>
-        </ul>
-        <!-- Messaggio d'errore -->
-        <p v-show="visible" class="paragrafo">Inserisci una città</p>
-        <!-- Bottone di ricerca -->
-        <router-link :to="{ name: 'search' }" @click="searchRequest()" class="buttonSearch">
-          <i class="fa-solid fa-magnifying-glass"></i>
-        </router-link>
-      </div>
-    </div>
-  </div>
-
-  <div class="cards-container" :class="{ 'opacity-zero': isLoading }">
-    <div class="row g-4 flex-wrap">
-      <div v-for="(apartment, i) in store.homepageContent" class="col-6 col-md-6 col-lg-4 col-xl-3">
-        <router-link :to="`/search/${apartment.id}`" class="card h-100">
-          <img :src="getImage(apartment.image)" class="card-img-top" alt="..." />
-          <div class="card-body">
-            <h5 class="card-title apartmentName">{{ apartment.title }}</h5>
-            <div class="row">
-              <div class="col-6 dettagli">
-                <p class="apartmentDetail">
-                  <i class="fas fa-door-open"></i> {{ apartment.rooms }}
-                  {{ getRightWords("Camere", apartment.rooms) }}
-                </p>
-                <p class="apartmentDetail">
-                  <i class="fas fa-bed"></i> {{ apartment.beds }}
-                  {{ getRightWords("Posti letto", apartment.beds) }}
-                </p>
-                <p class="apartmentDetail">
-                  <i class="fas fa-bath"></i> {{ apartment.bathrooms }}
-                  {{ getRightWords("bagni", apartment.bathrooms) }}
-                </p>
-              </div>
-            </div>
-
+  <header>
+    <nav class="container">
+      <div class="search-container">
+        <div class="search-bar">
+          <!-------------------- indirizzo --------------------->
+          <div class="suggerimenti-indirizzo">
+            <input
+              class=""
+              id="indirizzo"
+              list="suggestion"
+              type="text"
+              placeholder="Inserisci città"
+              v-model="inputIndirizzo"
+              @focus="isActive = true"
+              @blur="timeoutShow"
+              autocomplete="off"
+            />
+            <ul v-show="apiSuggestions.length > 0 && isActive == true">
+              <li
+                @click="writeAddress(singleAddress, $event)"
+                v-for="(singleAddress, i) in apiSuggestions"
+              >
+                <i class="fa-solid fa-location-dot"></i>
+                <span>{{ singleAddress }}</span>
+              </li>
+            </ul>
+            <p v-show="visible" class="paragrafo">L'indirizzo è obbligatorio</p>
           </div>
-        </router-link>
+          <!-------------------- Tasto ricerca --------------------->
+          <router-link
+            :to="{ name: 'search' }"
+            @click="searchRequest()"
+            class="buttonSearch"
+          >
+            <i class="fa-solid fa-magnifying-glass"></i>
+          </router-link>
+        </div>
       </div>
+      
+       
+      <!-- <p v-for="error in errors" class="paragrafo">{{ error }}</p> -->
+    </nav>
+  </header>
+  <hr/>
+  <div class="container">
+    <div class="card" v-for="apartment in store.homepageContent" :key="apartment.id">
+      <router-link :to="`/search/${apartment.id}`" class="card h-100 text-dark">
+    <img :src="apartment.image" width="100" class="card-img-top" :alt="apartment.image" />
+    <div class="card-body">
+      <h5 class="card-title">{{ apartment.title }}</h5>
+      <div class="row">
+        <div class="col-6 dettagli">
+          <p class="">
+            <i class="fas fa-door-open"></i> 
+            {{ apartment.rooms }}
+          </p>
+          <p class="">
+            <i class="fas fa-bed"></i> 
+            {{  apartment.beds}}
+          </p>
+          <p class="">
+            <i class="fas fa-bath"></i> 
+            {{  apartment.bathrooms }}
+          </p>
+        </div>
+      </div>
+      <hr />
+    </div>
+  </router-link>
+
     </div>
   </div>
-
-  <!-- <div class="card">
-      <img :src="image" class="card-img-top" alt="..." />
-      <div class="card-body">
-        <h5 class="card-title">{{ title }}</h5>
-        <p class="card-text">
-          Stanze:{{ rooms }}
-        </p>
-        <p class="card-text">
-          Posti letto:{{ beds }}
-        </p>
-        <p class="card-text">
-          Bagni:{{ bathrooms }}
-        </p>
-        <h5>Servizi:</h5>
-        <ul>
-
-          <li v-for="servizio in services">
-            {{ servizio.name }}
-          </li>
-        </ul>
-      </div>
-    </div> -->
-
 </template>
 
 <style scoped>
-:root {
-  --bool-color-1: white;
-  --bool-color-2: rgb(175, 175, 175);
-  --bool-color-3: rgb(99, 99, 99);
-  --bool-color-4: rgb(7, 7, 7);
+input {
+  all: unset;
 }
 
-HTML {
-  background-color: var(--bool-color-1);
-  color: var(--bool-color-4);
+header {
+  width: 100%;
+  min-height: 10rem;
+  display: flex;
+  align-items: center;
+  padding-top: 1rem;
+}
+
+.container {
+  position: relative;
 }
 
 .search-container {
   display: flex;
-  justify-content: space-between;
-  width: 20vw;
-  margin: 0 auto;
+  justify-content: center;
 }
 
 .search-bar {
   display: inline-flex;
-  height: 5rem;
+  height: 64.38px;
   background-color: white;
-  border-radius: 2.5rem;
-  padding: 0.2rem 1rem 0.2rem 2rem;
+  border-radius: 30px;
+  padding: 0.2rem 0rem 0.2rem 1rem;
   justify-content: center;
   text-align: center;
-  margin: 1rem auto;
+  margin-bottom: 0.5rem;
 }
 
 .buttonSearch {
   background-color: rgb(255, 255, 255);
   border-radius: 30px;
-  margin: 0.4px 0 0.4px 0.5rem;
+  margin: 0.4px 0;
   padding: 0 0.5rem 0 1rem;
   display: flex;
   align-items: center;
   cursor: pointer;
 }
 
-#città {
+#indirizzo {
   overflow: hidden;
   text-overflow: ellipsis;
   font-weight: 500;
-  font-size: 2rem;
-  border-radius: 2rem;
-  border: 0 white;
-  outline: none;
-}
-#città:focus {
-  border: 1px rgb(175, 175, 175);
+  width: 150px;
 }
 
-a {
-  text-decoration: none;
-  color: rgb(255, 255, 255);
-}
-
-a i {
-  background-color: #d8cfc4;
-  width: 50px;
-  height: 50px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  border-radius: 50%;
-  transition: 0.5s;
-}
-
-a i:hover {
-  background-color: #b3a49a;
-}
-
-
-.suggerimenti-città {
+.suggerimenti-indirizzo {
   display: flex;
   position: relative;
 }
 
-.suggerimenti-città i {
+.suggerimenti-indirizzo i {
   font-size: 40px;
   padding: 0.5rem;
 }
@@ -379,88 +351,194 @@ li:hover {
   background-color: #b3a49a;
   cursor: pointer;
 }
-.apartmentDetail, .apartmentName {
-  color: black;
-}
 
-/* Animazione di caricamento */
-.loader {
-  margin-top: 10rem;
-  width: 200px;
-  height: 200px;
+.number-logic {
+  position: relative;
   display: flex;
   justify-content: center;
   align-items: center;
-  position: relative;
+  width: 10rem;
 }
 
-.circle {
-  position: absolute;
-  width: 0px;
-  height: 0px;
-  border-radius: 100%;
-  background: rgb(25, 172, 221);
-  animation: radar 3s ease-out infinite;
-  box-shadow: 0px 0px 10px rgb(25, 172, 221);
+.label-number {
+  font-weight: 500;
 }
 
-.circle:nth-of-type(1) {
-  animation-delay: 0.2s;
+.label-range {
+  margin-bottom: 0.2rem;
 }
 
-.circle:nth-of-type(2) {
-  animation-delay: 0.6s;
-}
-
-.circle:nth-of-type(3) {
-  animation-delay: 1s;
-}
-
-.circle:nth-of-type(4) {
-  animation-delay: 1.4s;
-}
-
-.circle:nth-of-type(5) {
-  animation-delay: 1.8s;
-}
-
-@keyframes radar {
-  0% {}
-
-  30% {
-    width: 100px;
-    height: 100px;
-  }
-
-  100% {
-    width: 100px;
-    height: 100px;
-    opacity: 0;
-  }
-}
-
-.loading-screen {
-  position: fixed;
-  top: 40%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  opacity: 0;
-}
-
-.loading-show {
+.animation .number-logic {
+  display: unset;
   opacity: 100;
+  transition: 1s;
 }
 
-.loading-message {
-  position: fixed;
-  color: rgb(25, 172, 221);
-  width: 400px;
-  font-size: 2rem;
-  top: 30%;
-  left: -40%;
+#numero-stanze,
+#numero-letti {
+  width: 35px;
+  margin-left: 15px;
+  padding: 5px;
 }
 
-.opacity-zero {
-  opacity: 0;
+.span-km {
+  transform: translateX(-20px);
+}
+
+.button-plus,
+.button-minus {
+  border: 2px solid #d8cfc4;
+  background-color: white;
+  border-radius: 50%;
+  position: absolute;
+  transition: 0.5s;
+}
+
+#raggio-ricerca .button-plus {
+  right: 25px;
+}
+#raggio-ricerca .button-minus {
+  left: 25px;
+}
+
+.button-plus {
+  right: 35px;
+}
+
+.button-minus {
+  left: 35px;
+}
+
+.button-minus:hover,
+.button-plus:hover {
+  border-color: #8b8589;
+}
+
+.button-plus i,
+.button-minus i {
+  color: #d8cfc4;
+  transition: 0.3s;
+}
+
+.button-plus:active i,
+.button-minus:active i {
+  color: #8b8589;
+}
+
+.servizi {
+  margin-bottom: 0.3rem;
+  border: 3px solid rgb(255, 255, 255);
+  padding: 0.5rem 0.3rem;
+  border-radius: 20px;
+  cursor: pointer;
+  background-color: white;
+  transition: 0.4s;
+}
+
+.servizi:hover {
+  border-color: #b3a49a;
+}
+
+input[type='checkbox']:checked + .servizi {
+  border-color: #8b8589;
+}
+
+.paragrafo {
+  position: absolute;
+  top: 0rem;
+  color: red;
+  white-space: nowrap;
+}
+
+a {
+  text-decoration: none;
+  color: rgb(255, 255, 255);
+}
+
+a i {
+  background-color: #d8cfc4;
+  width: 50px;
+  height: 50px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 50%;
+  transition: 0.5s;
+}
+
+a i:hover {
+  background-color: #b3a49a;
+}
+
+hr {
+  border: 1px solid #b3a49a;
+  margin-top: 0;
+}
+
+/* range input style */
+/* range input style */
+#raggio {
+  -webkit-appearance: none;
+  appearance: none;
+  width: 100%;
+  height: 6px;
+  border-radius: 5px;
+  outline: none;
+  background: linear-gradient(
+    to right,
+    #91c2c5 var(--range-percentage),
+    #e0e0e0 var(--range-percentage)
+  );
+}
+
+#raggio:hover {
+  background: linear-gradient(
+    to right,
+    #91c2c5 var(--range-percentage),
+    #cfcfcf var(--range-percentage)
+  );
+}
+
+/* Track personalizzato */
+#raggio::-webkit-slider-runnable-track {
+  width: 100%;
+  height: 6px;
+  background: linear-gradient(
+    to right,
+    #91c2c5 var(--range-percentage),
+    #e0e0e0 var(--range-percentage)
+  );
+  border-radius: 5px;
+}
+
+/* Thumb personalizzato */
+#raggio::-webkit-slider-thumb {
+  -webkit-appearance: none;
+  appearance: none;
+  width: 12px;
+  height: 12px;
+  background: #9f8d7c; /* Colore tortora */
+  border-radius: 50%;
+  cursor: pointer;
+  margin-top: -3px;
+  border: none;
+}
+
+#raggio::-moz-range-thumb {
+  width: 12px;
+  height: 12px;
+  background: #9f8d7c;
+  border-radius: 50%;
+  cursor: pointer;
+  border: none;
+}
+
+#raggio::-ms-thumb {
+  width: 12px;
+  height: 12px;
+  background: #9f8d7c;
+  border-radius: 50%;
+  cursor: pointer;
+  border: none;
 }
 </style>
