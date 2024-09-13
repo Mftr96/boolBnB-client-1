ede4f53e7658da5be2aeaf9acde3effc17b2984d
lucianocretella95 <luciano.cretella@tim.it>
2024-09-13 18:04:04 +0200

    query footer da migliorare

--- src/components/Appfooter.vue
+++ src/components/Appfooter.vue
@@ -14,23 +14,17 @@ export default {
 <template>
   <footer class="app-footer">
     <div class="footer-content">
-      <p class="col30">&copy; 2024 Boolbnb, Tutti i diritti riservati.</p>
-      <ul class="col40">
+      <p class="col-md-3">&copy; 2024 Boolbnb, Tutti i diritti riservati.</p>
+      <ul class="col-md-6">
         <a href="/chi-siamo">Chi siamo</a>
         <a href="/contatti">Contatti</a>
         <a href="/privacy-policy">Privacy Policy</a>
         <a href="/termini-condizioni">Termini e condizioni</a>
       </ul>
-      <div class="social-media col30">
-        <a href="https://www.facebook.com/login/?locale=it_IT"
-          ><i class="fa-brands fa-square-facebook"></i
-        ></a>
-        <a href="https://x.com/i/flow/login"
-          ><i class="fa-brands fa-x-twitter"></i
-        ></a>
-        <a href="https://www.instagram.com/accounts/login/"
-          ><i class="fa-brands fa-instagram"></i
-        ></a>
+      <div class="social-media col-md-3">
+        <a href="https://www.facebook.com/login/?locale=it_IT"><i class="fa-brands fa-square-facebook"></i></a>
+        <a href="https://x.com/i/flow/login"><i class="fa-brands fa-x-twitter"></i></a>
+        <a href="https://www.instagram.com/accounts/login/"><i class="fa-brands fa-instagram"></i></a>
       </div>
     </div>
   </footer>
@@ -40,8 +34,8 @@ export default {
 p,
 a,
 i {
-  margin-left: 0.5rem;
-  margin-right: 0.5rem;
+  margin-left: 0.2rem;
+  margin-right: 0.2rem;
 }
 
 i {
@@ -70,43 +64,49 @@ a {
   justify-content: space-between;
   align-items: center;
   padding: 0.5rem;
-  width: 1600px;
 }
-.footer-content > p,
-.footer-content > ul {
+
+.footer-content>p,
+.footer-content>ul {
   margin: 0;
 }
-.footer-content > ul > a {
+
+.footer-content>ul>a {
   border-radius: 5px;
-  padding: 0.35rem;
+  padding: 0.5rem;
   margin: 0 0.2rem;
 }
-.footer-content > ul > a:hover {
+
+.footer-content>ul>a:hover {
   background-color: rgb(20, 20, 20);
 }
-.col30 {
+
+/* .col30 {
   width: 30%;
 }
 .col40 {
   width: 40%;
-}
+} */
 
 .social-media {
   text-align: end;
 }
-.social-media > a {
+
+.social-media>a {
   position: relative;
   display: inline-block;
   width: 2rem;
   height: 2rem;
   border-radius: 5px;
 }
-.social-media > a > i {
+
+.social-media>a>i {
   position: absolute;
   top: 0.5rem;
   left: 0rem;
 }
-.social-media > a:hover {
+
+.social-media>a:hover {
   background-color: rgb(20, 20, 20);
 }
 </style>
