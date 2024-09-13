1fc74267d08a99c65b76f0f2f0d178236ff6ca26
Dihep <dihep.demayo@gmail.com>
2024-09-13 07:45:30 +0200

    Piccole migliorie per il Footer.

--- src/components/Appfooter.vue
+++ src/components/Appfooter.vue
@@ -14,14 +14,14 @@ export default {
 <template>
   <footer class="app-footer">
     <div class="footer-content">
-      <p class="col33">&copy; 2024 Boolbnb, Tutti i diritti riservati.</p>
-      <ul class="col33">
+      <p class="col30">&copy; 2024 Boolbnb, Tutti i diritti riservati.</p>
+      <ul class="col40">
         <a href="/chi-siamo">Chi siamo</a>
         <a href="/contatti">Contatti</a>
         <a href="/privacy-policy">Privacy Policy</a>
         <a href="/termini-condizioni">Termini e condizioni</a>
       </ul>
-      <div class="social-media col33">
+      <div class="social-media col30">
         <a href="https://www.facebook.com/login/?locale=it_IT"
           ><i class="fa-brands fa-facebook"></i
         ></a>
@@ -70,12 +70,41 @@ a {
   padding: 0.5rem;
   width: 1600px;
 }
-
-.col33 {
-  width: 33%;
+.footer-content > p, .footer-content > ul {
+  margin: 0;
+}
+.footer-content > ul > a {
+  border-radius: 1rem;
+  padding: 0.35rem;
+  margin: 0 0.2rem;
+}
+.footer-content > ul > a:hover {
+  background-color: rgb(20, 20, 20);
+}
+.col30 {
+  width: 30%;
+}
+.col40 {
+  width: 40%;
 }
 
 .social-media {
   text-align: end;
 }
+.social-media > a {
+  position: relative;
+  display: inline-block;
+  width: 2rem;
+  height: 2rem;
+  border-radius: 1rem;
+}
+.social-media > a > i {
+  position: absolute;
+  top: 0.5rem;
+  left: 0rem;
+}
+.social-media > a:hover {
+  background-color: rgb(20, 20, 20);
+}
+
 </style>
